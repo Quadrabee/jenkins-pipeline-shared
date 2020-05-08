0b222fdcba7d61b71d545bb13d510ab39045144f
Louis Lambeau <llambeau@quadrabee.com>
2020-05-08 22:25:41 +0200

    fixup! Test

--- vars/newTest.groovy
+++ vars/newTest.groovy
@@ -5,6 +5,7 @@ import java.util.Optional
 import hudson.tasks.test.AbstractTestResultAction
 import hudson.model.Actionable
 import hudson.tasks.junit.CaseResult
+import com.quadrabee.utils
 
 def call(String channel) {
   def utils = new com.quadrabee.utils()
